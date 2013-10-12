afbf1fcd173c33701db93fa78de9eff33aadd63f
Matthew M. Boedicker <matthewm@boedicker.org>
2013-10-12 00:12:53 -0700

    Add http listen address command line arg.

--- amqpcastd.go
+++ amqpcastd.go
@@ -12,6 +12,7 @@ func main() {
 		"AMQP server URL")
 	var amqpExchange = flag.String("amqp-exchange", "test", "AMQP exchange")
 	var amqpKey = flag.String("amqp-key", "test", "AMQP routing key")
+	var httpListen = flag.String("http-listen", ":12345", "HTTP listen host and port")
 
 	flag.Parse()
 
@@ -22,7 +23,7 @@ func main() {
 		Outbound:    make(chan string, 256),
 	}
 
-	amqpcast.InitHttp(&cstr)
+	amqpcast.InitHttp(httpListen, &cstr)
 	amqpcast.InitAmqp(amqpUrl, amqpExchange, amqpKey, &cstr)
 
 	for {
