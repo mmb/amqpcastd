1c084949e9914ae94d6730ae1d3272010a5bfd09
Matthew M. Boedicker <matthewm@boedicker.org>
2013-12-14 02:01:54 -0800

    Use :=

--- amqpcastd.go
+++ amqpcastd.go
@@ -7,11 +7,10 @@ import (
 )
 
 func main() {
-	var amqpUrl = flag.String("amqp-url", "amqp://localhost:5672/",
-		"AMQP server URL")
-	var amqpExchange = flag.String("amqp-exchange", "test", "AMQP exchange")
-	var amqpKey = flag.String("amqp-key", "test", "AMQP routing key")
-	var httpListen = flag.String("http-listen", ":12345", "HTTP listen host and port")
+	amqpUrl := flag.String("amqp-url", "amqp://localhost:5672/", "AMQP server URL")
+	amqpExchange := flag.String("amqp-exchange", "test", "AMQP exchange")
+	amqpKey := flag.String("amqp-key", "test", "AMQP routing key")
+	httpListen := flag.String("http-listen", ":12345", "HTTP listen host and port")
 
 	flag.Parse()
 
